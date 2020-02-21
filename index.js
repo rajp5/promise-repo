3262d54170e1a8d3a84a06492da84da8f8b4a028
admin <admin@USCS-Mac258.local>
2020-02-21 15:09:06 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('Start');
+
+function getUser(id){
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=>{
+            console.log('User with id: '+id)
+            resolve({id: id, username:'xyz'})
+        },2000)
+    })
+};
+
+function getRepos(id, username){
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=>{
+            console.log(id, username)
+            resolve(['repo1','repo2'])
+        },2000)
+    })
+};
+
+function getCommits(repo){
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=>{
+            console.log(repo)
+            resolve(console.log('Commits'))}
+        ,2000)
+    })
+};
+
+getUser(1)
+    .then((user)=> getRepos(user.id, user.username))
+    .then((repos)=> getCommits(repos[0]))
+    .then((commits)=>console.log(commits));
+
+    console.log('End');
